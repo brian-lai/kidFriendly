867ad6ec3cf14ff71499590853fd579329a65c40
Brian Lai <brian.lai.1992@gmail.com>
2015-02-27 13:53:51 -0600

    Test Code

--- kidFriendlyPlaces.js
+++ kidFriendlyPlaces.js
@@ -1,1 +1,11 @@
-document.getElementsByClassName("")
+var list = []
+var manhattan = document.getElementsByClassName("menu__item menu-1253 menuparent  menu-path-nolink first odd")
+
+var downtown = manhattan[0].getElementsByTagName("li")
+
+var batteryPark = downtown[1].getElementsByTagName("a")
+
+var batteryParkAddress = batteryPark[0].attributes.href.value
+
+
+location.assign(batteryParkAddress)
